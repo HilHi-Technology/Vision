df1fc4740c3b5074af7322d8d95529b7f87bc23a
Jonathan Alexander <notdisliked@gmail.com>
2017-02-22 13:41:14 -0800

    Initial commit

---
+++ Assets/Scripts/ViewPortScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+
+public class ViewPortScript : MonoBehaviour {
+
+    public GameObject ViewPort;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        GameObject headset = GameObject.FindGameObjectWithTag("MainCamera");
+        float distanceToPort = Vector3.Distance(headset.transform.position, ViewPort.transform.position);
+        float fovAngle =  Mathf.Atan((ViewPort.transform.lossyScale.x / 2) / distanceToPort) * Mathf.Rad2Deg * 2;
+        print(Mathf.Atan((ViewPort.transform.lossyScale.x / 2) / distanceToPort) * Mathf.Rad2Deg * 2);
+        GetComponent<Camera>().fieldOfView = fovAngle;
+	}
+}
