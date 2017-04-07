7c82e075821e2ccedaa1c3774bd4fc6f97e2c355
Jonathan Alexander <notdisliked@gmail.com>
2017-04-07 00:32:30 -0700

    Wensday changes

--- Assets/Scripts/Level1/MoveSphereScript.cs
+++ Assets/Scripts/Level1/MoveSphereScript.cs
@@ -5,6 +5,8 @@ using Valve.VR;
 
 public class MoveSphereScript : MonoBehaviour {
 
+    public GameObject detectionObject;
+
     private HmdQuad_t rect;
     private float zPos = -1.5f;
     private float vel = 0.025f;
@@ -21,7 +23,7 @@ public class MoveSphereScript : MonoBehaviour {
         if (zPos > 1.25f) {
             zPos = -1.25f;
         }
-        if (InVision.CanSee(gameObject)) {
+        if (InVision.CanSee(detectionObject)) {
             vel += 0.00025f;
             if (vel > 0.025f) {
                 vel = 0.025f;
